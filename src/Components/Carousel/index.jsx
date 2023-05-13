170b773cb5761c7faaa589745710d1cc69c2ec7a
thallysvic17 <thallysvic17@gmail.com>
2023-05-13 12:01:33 -0300

    Update index.jsx

--- src/Components/Carousel/index.jsx
+++ src/Components/Carousel/index.jsx
@@ -6,7 +6,7 @@ import Slider from "react-slick";
 
 
 import img2 from '../../assets/images/Carousel-img/img2.jpg';
-import img1 from '../../assets/images/Carousel-img/img1.jpg';
+//import img1 from '../../assets/images/Carousel-img/img1.jpg';
 import img3 from '../../assets/images/Carousel-img/img3.jpg';
 import img4 from '../../assets/images/Carousel-img/img4.jpg';
 
@@ -15,7 +15,7 @@ import img4 from '../../assets/images/Carousel-img/img4.jpg';
 
 const Carousel = () => {
   
-  const slides = [img1, img2, img3, img4];
+  const slides = [ img2, img3, img4];
 
   const settings = {
     dots: true,
