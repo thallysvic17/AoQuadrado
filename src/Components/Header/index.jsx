3b90a3720919c189162a16bb65ffb042d5a39396
thallysvic17 <thallysvic17@gmail.com>
2023-05-13 16:06:40 -0300

    Update index.jsx

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,15 +1,21 @@
 import {HeaderStyle, Logo,NavMenu} from './styled'
 
 
-
+import logo from '../../assets/Images/logos/logosite.jpeg'
 
 
 const Header = () =>{
 
+
+ const headerlogo =[{
+    id:1, src: logo
+ }]
+
     return(
         <HeaderStyle>
             <Logo >
-                <img className="img" src= 'src\assets\Images\logos\logosite.jpeg' alt="Logo site" />
+                {headerlogo.map((logo) => (
+                <img key={logo.id} src={logo.src} alt='Logo site' className="img"/>))}
                 <div className="lg">
                     <h1>Ao quadrado</h1>
                     <p>studio de arquitetura</p>
